6492be49edfdbc7dfc8500e45705a18fef494bb1
DuvCabrera <eduardoviniciuscabrera@gmail.com>
2021-08-22 01:02:00 -0300

    started and finished class js

---
+++ JS/js/main.js
@@ -0,0 +1,91 @@
+
+
+function agradecimento() {
+    document.getElementById("agradecimento").innerHTML = "Obrigado por clicar";
+}
+
+function redirecionar() {
+    window.open("https://web.digitalinnovation.one/course/programacao-para-internet-com-javascript/learning/ddb54ad6-55fb-4eec-90d5-561a6010d14d?back=/track/html-web-developer")
+    //window.location.href =" https://web.digitalinnovation.one/course/programacao-para-internet-com-javascript/learning/ddb54ad6-55fb-4eec-90d5-561a6010d14d?back=/track/html-web-developer" 
+}
+
+function passarMouse(elemento) {
+    elemento.innerHTML = "Obrigado por passar o mouse"
+
+}
+
+function voltar (elemento) {
+    elemento.innerHTML = "Passe o mouse aqui"
+}
+
+function load() {
+    alert("pagina carregada")
+}
+
+function changeValue(elemento) {
+    console.log(elemento.value)
+}
+/*function botao() {
+    alert("obricado por clicar")
+}*/
+
+
+
+
+/*function soma (a , b) {
+    return a+b;
+}*/
+
+//var d = new Date();
+//alert(d.getDay)
+
+/*var count;
+for ( count=0; count <=5; count++){
+    alert(count);
+}*/
+
+/*var count = 0;
+while (count <= 5) {
+    console.log(count)
+    count += 1
+}*/
+
+
+/*var idade = prompt ("Qual é sua idade? ")
+if (idade >= 18) {
+    alert("maior de idade");
+} else {
+    alert("menor de idade");
+}*/
+
+
+//var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+//console.log(frutas)
+
+
+//var fruta = {nome:"maça", cor:"vermelha"}
+//console.log(fruta)
+
+
+//var lista = ["maça", "pera", "laranja"];
+//lista.push("uva"); /* add item na lista */
+//lista.pop() /*tira o item da lista*/
+
+//lista.length() /* mostra o tamanho da lista */
+
+//lista.reverse() /* inverte a localizaçao dos itens */
+
+//lista.toString() /* converte a lista para string */
+
+//lista.join("-") /* troca o caracter que separa os elementos da lista por outro e transforma em string */
+
+/*var idade = 30;
+var nome = " Eduardo Cabrera";
+alert (nome + " tem " + idade);
+var idade2 = 10;
+var frase= "Japão é o melhor time do mundo"
+//alert(idade + idade2);
+
+console.log (frase.replace("Japão", "Brasil"))
+console.log(nome)
+console.log (idade + idade2)*/
--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Fist-class - primeira aula do bootcamp
 FlexBox - Aula onde Clonaremos a pagina de login do instagram.
 
 - DisplayFlex - quando setado em um container o mesmo se torna um flexcontainer onde todos os itens localizados dentro dele serão organizados em linhas.
+
+JS - JavaScript aulas basicas
